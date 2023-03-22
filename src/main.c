f72ca12ea5465e8463a12b8e8fe47716522a672e
TIny_Hacker <thetiniesthacker@gmail.com>
2023-03-22 14:48:55 -0400

    Fix bug with icon offset
    Co-Authored-By: RoccoLox Programs <roccoloxprograms2018@gmail.com>

--- src/main.c
+++ src/main.c
@@ -5,8 +5,8 @@
  * By RoccoLox Programs and TIny_Hacker
  * Copyright 2022 - 2023
  * License: GPL-3.0
- * Last Build: March 1, 2023
- * Version: 1.1.1
+ * Last Build: March 22, 2023
+ * Version: 1.1.2
  * 
  * --------------------------------------
 **/
