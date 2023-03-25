6e3a3d1615afaf48090f345a28f93260e35676ad
TIny_Hacker <thetiniesthacker@gmail.com>
2023-03-25 07:30:32 -0400

    Reinstall icons when program errors out
    Co-Authored-By: RoccoLox Programs <roccoloxprograms2018@gmail.com>

--- src/main.c
+++ src/main.c
@@ -5,8 +5,8 @@
  * By RoccoLox Programs and TIny_Hacker
  * Copyright 2022 - 2023
  * License: GPL-3.0
- * Last Build: March 22, 2023
- * Version: 1.1.2
+ * Last Build: March 24, 2023
+ * Version: 1.1.5
  * 
  * --------------------------------------
 **/
